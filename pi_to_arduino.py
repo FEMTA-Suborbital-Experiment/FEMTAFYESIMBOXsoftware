d9031aa526931d2fbe3779b1b66f424659e2fdc8
lavzed <72566071+lavzed@users.noreply.github.com>
2020-10-20 15:54:21 -0400

    Add files via upload

---
+++ pi_to_arduino.py
@@ -0,0 +1,15 @@
+import serial
+import time
+
+
+if __name__ == '__main__':
+    s = serial.Serial('/dev/serial0', baudrate=115200, timeout=1)
+    time.sleep(1)
+    msg = b'Hello from the Raspberry pi'
+
+    s.flush()  # This prevents other inputs except msg
+    while True:
+        s.write(msg)
+        output = s.readline().decode()
+        print(output)
+        time.sleep(1)
