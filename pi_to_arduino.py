8ea45389174e6e074caf58dbe6279da9193891e9
LavZed <jacob.a.valdez2002@gmail.com>
2020-10-20 16:20:23 -0400

    IS THIS REALLY IT??

--- pi_to_arduino.py
+++ pi_to_arduino.py
@@ -10,5 +10,5 @@ if __name__ == '__main__':
     while True:
         s.write(msg)
         output = s.readline().decode().rstrip()
-        # print(output)
+        print(output)
         time.sleep(1)
