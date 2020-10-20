1e87b944ce6d0e71192408ef29a770f47e964082
LavZed <jacob.a.valdez2002@gmail.com>
2020-10-20 16:20:02 -0400

    IS THIS REALLY IT??

--- pi_to_arduino.py
+++ pi_to_arduino.py
@@ -4,11 +4,11 @@ import time
 
 if __name__ == '__main__':
     s = serial.Serial('/dev/ttyACM0', baudrate=115200, timeout=1)
-    msg = b'Hello from the Raspberry pi'
+    msg = b'HHello from the Raspberry pi'
 
     s.flush()  # This prevents other inputs except msg
     while True:
         s.write(msg)
         output = s.readline().decode().rstrip()
-        print(output)
+        # print(output)
         time.sleep(1)
