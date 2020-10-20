d592ea58c67fcaac821329e4c73ff7c46c80e2c6
evanrittner <evanrittner@gmail.com>
2020-10-20 18:28:36 -0400

    Update pi_to_arduino.py

--- pi_to_arduino.py
+++ pi_to_arduino.py
@@ -1,14 +1,15 @@
 import serial
 import time
 
-
 if __name__ == '__main__':
     s = serial.Serial('/dev/ttyACM0', baudrate=115200, timeout=1)
-    msg = b'H'
-
+    data = [0,1,2,3,4,5,6,7,8,9]
+    i = 0
+    
     s.flush()  # This prevents other inputs except msg
     while True:
-        s.write(msg)
+        s.write(data[i])
+        i += 1
         output = s.readline()
         print(output)
         time.sleep(1)
