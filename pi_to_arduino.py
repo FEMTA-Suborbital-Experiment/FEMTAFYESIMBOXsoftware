f4d3ee1dd3d783e008c2800d5d199bf84eade407
LavZed <jacob.a.valdez2002@gmail.com>
2020-10-20 16:51:23 -0400

    IS THIS REALLY IT??

--- pi_to_arduino.py
+++ pi_to_arduino.py
@@ -4,7 +4,7 @@ import time
 
 if __name__ == '__main__':
     s = serial.Serial('/dev/ttyACM0', baudrate=115200, timeout=1)
-    msg = 420
+    msg = b'h'
 
     s.flush()  # This prevents other inputs except msg
     while True:
