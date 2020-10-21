3245b84509dec3c4c4830b43060ec0a632835250
evanrittner <evanrittner@gmail.com>
2020-10-20 22:46:40 -0400

    Renaming files to be more specific

---
+++ arduino testing/serial_integer_test/serial_integer_test.h
@@ -0,0 +1,6 @@
+#ifndef SERIAL_INTEGER_TEST.H
+#define SERIAL_INTEGER_TEST.H
+
+#include <Wire.h> //  Arduino library for twi interface
+
+#endif //SERIAL_INTEGER_TEST.H
--- arduino testing/twi_slave_emulator/twi_slave_emulator.h
+++
@@ -1,8 +0,0 @@
-
-
-#ifndef TWI_SLAVE_EMULATOR.H
-#define TWI_SLAVE_EMULATOR.H
-
-#include <Wire.h> //  Arduino library for twi interface
-
-#endif //TWI_SLAVE_EMULATOR.H
