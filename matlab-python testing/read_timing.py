8343c3d0852da6f5cbc7da7ebe0bb74e0a8ccce4
evanrittner <evanrittner@gmail.com>
2020-10-22 16:38:17 -0400

    .

--- matlab-python testing/read_timing.py
+++ matlab-python testing/read_timing.py
@@ -1,12 +1,8 @@
 import datetime
 now = datetime.datetime.now
-from random import random
-
-newdata = [round(random() * 1000, 4) for i in range(12)]
 
 t1 = now()
-with open("sample_data.csv", "w") as f:
-    for val in newdata:
-        f.write(str(val) + " ")
+with open("sample_data.csv", "r") as f:
+    data = readline()
 t2 = now()
 print(t2.microsecond - t1.microsecond)
