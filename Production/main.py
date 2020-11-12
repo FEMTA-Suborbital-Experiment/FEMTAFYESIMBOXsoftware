ad67c5f9dd757f8681c1de01b09f5253807eb479
evanrittner <evanrittner@gmail.com>
2020-11-11 19:32:37 -0500

    Update main.py

--- Production/main.py
+++ Production/main.py
@@ -12,6 +12,7 @@ import serial           #USB
 #import socket           #Ethernet (and Matlab?)
 #import RPi.GPIO as GPIO #GPIO (valve feedback)
 from flow_conversion import flow_to_bytes
+from add_noise import fuzz
 
 
 #Define addresses
@@ -28,14 +29,10 @@ GPIO_PINS = (7, 11, 15, 18, 23, 25)
 
 
 #Define calibrations (units (which?) -> voltage)
-p0_cal = lambda x: 0.2698*x + 0.1013
-p1_cal = lambda x: 0.2462*x + 0.4404
-p2_cal = lambda x: 0.2602*x + 0.1049
-#p3_cal = lambda x: ???
-#t0_cal = lambda x:
-#t1_cal = lambda x:
-#t2_cal = lambda x:
-#t3_cal = lambda x:
+pres_cals = (lambda x: 0.2698*x + 0.1013, lambda x: 0.2462*x + 0.4404,
+             lambda x: 0.2602*x + 0.1049, lambda x: 0)
+therm_cals = (lambda x: 0, lambda x: 0,
+              lambda x: 0, lambda x: 0)
 
 
 #Set up connections
@@ -49,35 +46,41 @@ for pin in GPIO_PINS:
 
 while True: #While loop for now; look into executing on interval (timeloop lib?)
     #Receive sensor data from Matlab
-    [pres0, pres1, pres2, pres3, therm0, therm1, therm2, therm3,
-     dig_flow0, dig_flow1, dig_temp0, dig_temp1, ir_flow0, ir_flow1] = matlab.read()
+    sensor_data = list(matlab.read())
+    #pres0, pres1, pres2, pres3, therm0, therm1, therm2, therm3,
+    #dig_flow0, dig_flow1, dig_temp0, dig_temp1, ir_flow0, ir_flow1
     
     #Process data
-    add_noise(all_sensors)
-    uncalibrate(pressure)
-    uncalibrate(thermistors)
+    sensor_data = [fuzz(d) for d in sensor_data]
+    for i in range(4):
+        sensor_data[i] = pres_cals[i](sensor_data[i])
+        sensor_data[i+4] = therm_cals[i](sensor_data[i+4])
+        
+    #Make fake UV and mass spec. data
+    uva, uvb, uvc1, uvc2, uvd = make_fake_uv()
+    mass0, mass1 = make_fake_ms()
     
     #Prepare digital data to send to Arduino
     #9 bytes for each flow, 10 for UV, 1 for error state, 1 for terminating \n
-    f0_data = flow_conversion(dig_flow0, dig_temp0)
-    f1_data = flow_conversion(dig_flow1, dig_temp1)
-    uv_data = uv_conversion(uv)
+    f0_data = flow_conversion(sensor_data[8], sensor_data[10])
+    f1_data = flow_conversion(sensor_data[9], sensor_data[11])
+    uv_data = uv_conversion(uva, uvb, uvc1, uvc2, uvd)
     digital_data = [*f0_data, *f1_data, *uv_data, ERROR_STATE, 0x0a]
     
     #Output data
-    i2c.write_i2c_block_data(DAC[0], P[0], pres0)
-    i2c.write_i2c_block_data(DAC[0], P[1], pres1)
-    i2c.write_i2c_block_data(DAC[0], P[2], pres2)
-    i2c.write_i2c_block_data(DAC[0], P[3], pres3)
-    i2c.write_i2c_block_data(DAC[0], T[0], therm0)
-    i2c.write_i2c_block_data(DAC[0], T[1], therm1)
-    i2c.write_i2c_block_data(DAC[0], T[2], therm2)
-    i2c.write_i2c_block_data(DAC[0], T[3], therm3)
+    i2c.write_i2c_block_data(DAC[0], P[0], sensor_data[0])
+    i2c.write_i2c_block_data(DAC[0], P[1], sensor_data[1])
+    i2c.write_i2c_block_data(DAC[0], P[2], sensor_data[2])
+    i2c.write_i2c_block_data(DAC[0], P[3], sensor_data[3])
+    i2c.write_i2c_block_data(DAC[0], T[0], sensor_data[4])
+    i2c.write_i2c_block_data(DAC[0], T[1], sensor_data[5])
+    i2c.write_i2c_block_data(DAC[0], T[2], sensor_data[6])
+    i2c.write_i2c_block_data(DAC[0], T[3], sensor_data[7])
     
     i2c.write_i2c_block_data(DAC[1], MS[0], mass0)
     i2c.write_i2c_block_data(DAC[1], MS[1], mass0)
-    i2c.write_i2c_block_data(DAC[1], IR[0], ir_flow0)
-    i2c.write_i2c_block_data(DAC[1], IR[1], ir_flow1)
+    i2c.write_i2c_block_data(DAC[1], IR[0], sensor_data[12])
+    i2c.write_i2c_block_data(DAC[1], IR[1], sensor_data[13])
     
     arduino.write(bytes(digital_data))
     
