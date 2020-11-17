17362d3179cd94ad9b5e9a206094afccdcf05948
evanrittner <evanrittner@gmail.com>
2020-11-17 10:22:19 -0500

    Create timeloop_test.py

---
+++ Pi testing/timeloop_test.py
@@ -0,0 +1,25 @@
+from datetime import datetime, timedelta
+now = datetime.now
+from timeloop import Timeloop
+from math import sqrt
+
+def raw_times():
+    for i in range(10):
+        t0 = now()
+        [sqrt(i) for i in range(1000000)]
+        t1 = now()
+        print(t1 - t0)
+   
+   
+tl = Timeloop()
+prev = now()
+
+@tl.job(interval=timedelta(seconds=2))
+def test_func():
+    global prev
+    print(now() - prev)
+    prev = now()
+    [sqrt(i) for i in range(1000000)]
+    
+def tloop():
+    tl.start(block=True)
