c57b2875f53a5b97c75dcc642b6c81833d2448da
evanrittner <evanrittner@gmail.com>
2020-11-18 13:44:19 -0500

    Tweak with lab DAC testing

--- Pi testing/dac_output_test.py
+++ Pi testing/dac_output_test.py
@@ -1,5 +1,9 @@
-import smbus
+import board
+import busio
+import time
+
 import math #just for output sine
+
 
 address = 0x28 #DAC #1
 register = 0x4 #channel 4 (thermistor)
@@ -8,6 +12,7 @@ bus = smbus.SMBus(1)
 
 i = 0
 while True:
-    msg = round(128 * (math.sin(i/10) + 1))
-    bus.write_i2c_block_data(address, register, msg)
+    #msg = math.floor(128 * (math.sin(i/100) + 1))
+    bus.write_i2c_block_data(address, 0, [register, 128 * (i%2)])
     i += 1
+    time.sleep(2)
