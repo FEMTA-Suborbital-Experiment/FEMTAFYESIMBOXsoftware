8675974e768951d9f738445a2a0b41a77582c98f
evanrittner <evanrittner@gmail.com>
2020-11-18 17:53:41 -0500

    Fixed DAC test script

--- Pi testing/dac_output_test.py
+++ Pi testing/dac_output_test.py
@@ -1,18 +1,16 @@
-import board
-import busio
+from busio import I2C
 import time
 
 import math #just for output sine
 
-
 address = 0x28 #DAC #1
 register = 0x4 #channel 4 (thermistor)
 
-bus = smbus.SMBus(1)
+i2c = I2C(5,3)
 
 i = 0
 while True:
-    #msg = math.floor(128 * (math.sin(i/100) + 1))
-    bus.write_i2c_block_data(address, 0, [register, 128 * (i%2)])
+    msg = math.floor(128 * (math.sin(i/100) + 1))
+    i2c.writeto(address, [register, msg])
     i += 1
-    time.sleep(2)
+    time.sleep(0.01)
