fcb5972d2cd84ce38854664323f86d44ec894bdc
evanrittner <evanrittner@gmail.com>
2020-11-18 15:03:50 -0500

    Organizing some files into proper folders

---
+++ Pi testing/pi_to_arduino.py
@@ -0,0 +1,14 @@
+import serial
+import time
+
+
+if __name__ == '__main__':
+    s = serial.Serial('/dev/ttyACM0', baudrate=115200, timeout=1)
+    msg = b'h'
+
+    s.flush()  # This prevents other inputs except msg
+    while True:
+        s.write(msg)
+        output = s.readline()
+        print(output)
+        time.sleep(0.2)
