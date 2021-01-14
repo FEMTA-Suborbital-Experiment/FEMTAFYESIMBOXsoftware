12964a272323a6e51a61ef0f8c3e8cae9288d993
Ethan Kessel <eqkessel@gmail.com>
2021-01-14 15:10:06 -0500

    Refined TWI Emulator tuned specifically for sensor applications

--- arduino testing/twi_slave_emulator/twi_slave_emulator.h
+++ arduino testing/twi_slave_emulator/twi_slave_emulator.h
@@ -1,40 +1,38 @@
-////////////////////////////////////////////////////////////////////////////////////
-//  
-//  twi_slave_emulator.h
-//  
-//  Written by Ethan Kessel (mailto:eqkessel@gmail.com).
-//  
-//  Enables safe, scalable, and trackable use of an Arduino to act as multiple slaves on the same
-//  Two-Wire Interface bus utilizing the Wire.h library.
-//  
-//  This software is loosely based on a simpler TWI simulator written by github user alexisgaziello
-//  https://github.com/alexisgaziello/TwoWireSimulator
-//
-////////////////////////////////////////////////////////////////////////////////////
-//
-//  MIT License
-//  
-//  Copyright (c) 2020 Ethan Kessel
-//  
-//  Permission is hereby granted, free of charge, to any person obtaining a copy
-//  of this software and associated documentation files (the "Software"), to deal
-//  in the Software without restriction, including without limitation the rights
-//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-//  copies of the Software, and to permit persons to whom the Software is
-//  furnished to do so, subject to the following conditions:
-//  
-//  The above copyright notice and this permission notice shall be included in all
-//  copies or substantial portions of the Software.
-//  
-//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-//  SOFTWARE.
-//
-////////////////////////////////////////////////////////////////////////////////////
+/****************************************************************************************************
+
+twi_slave_emulator.h
+
+Written by Ethan Kessel (mailto:eqkessel@gmail.com).
+
+Enables safe, scalable, and trackable use of an Arduino to act as multiple slaves on the same
+Two-Wire Interface bus utilizing the Wire.h library.
+
+This software is loosely based on a simpler TWI simulator written by github user alexisgaziello
+https://github.com/alexisgaziello/TwoWireSimulator
+
+MIT License
+
+Copyright (c) 2020 Ethan Kessel
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+****************************************************************************************************/
 
 #ifndef TWI_SLAVE_EMULATOR_H  //  #include protection
 #define TWI_SLAVE_EMULATOR_H
