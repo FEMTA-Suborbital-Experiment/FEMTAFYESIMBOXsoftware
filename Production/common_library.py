2eb4d943d2565ef3d57f1e0ba3f6574c87cad070
evanrittner <evanrittner@gmail.com>
2021-01-20 21:09:01 -0500

    Couple of tweaks for UV functions
    Added header, defined integration times, made generators work

--- Production/common_library.py
+++ Production/common_library.py
@@ -1,2 +1,4 @@
+ # Collection of commonly-used functions across multiple modules.
+
 def twos_comp(num):
     return num if num >= 0 else 65536 + num
--- Production/uv_conversion.py
+++ Production/uv_conversion.py
@@ -1,8 +1,12 @@
-from common_library import twos_comp
+# Functions to generate meaningless UV sensor data (sinusoids) and to
+# convert that data to the expected format
+
 from math import sin
 from random import randrange
+from common_library import twos_comp
 
 AMP_DIFF = 100
+STEP_SIZE = 0.1
 
 UVA_AMP = randrange(8000+AMP_DIFF)
 UVB_AMP = randrange(10000+AMP_DIFF)
@@ -16,18 +20,15 @@ def sine_generator(step_size, amplitude):
     while True:
         yield amplitude + amplitude * sin(x)
         x += step_size
-
+        
+uva = sine_generator(STEP_SIZE, UVA_AMP)
+uvb = sine_generator(STEP_SIZE, UVB_AMP)
+uvc1 = sine_generator(STEP_SIZE, UVC1_AMP)
+uvc2 = sine_generator(STEP_SIZE, UVC2_AMP)
+uvd = sine_generator(STEP_SIZE, UVD_AMP)
 
 def make_fake_uv():
-
-    uva = sine_generator(0.5, UVA_AMP)
-    uvb = sine_generator(0.5, UVB_AMP)
-    uvc1 = sine_generator(0.5, UVC1_AMP)
-    uvc2 = sine_generator(0.5, UVC2_AMP)
-    uvd = sine_generator(0.5,UVD_AMP)
-
-    return uva, uvb, uvc1, uvc2, uvd
-
+    return next(uva), next(uvb), next(uvc1), next(uvc2), next(uvd)
 
 def uv_conversion(uva, uvb, uvc1, uvc2, uvd):
     # CONSTANTS, for open-air systems with and without a teflon diffusor over the VEML6075
@@ -40,9 +41,9 @@ def uv_conversion(uva, uvb, uvc1, uvc2, uvd):
     powered_on = 1
 
     # Set integration times for UVA, UVB, and UVD, in ms
-    uv_int1 =
-    uv_int2 =
-    uv_int3 =
+    uv_int1 = 0
+    uv_int2 = 0
+    uv_int3 = 0
 
     # Scale UVA data to a 16-bit resolution
 
