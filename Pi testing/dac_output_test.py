f4e2eb89982f4ff0d45482d3b63f7de00a9aef98
evanrittner <evanrittner@gmail.com>
2021-01-25 17:41:18 -0500

    Updated DAC test comments

--- Pi testing/dac_output_test.py
+++ Pi testing/dac_output_test.py
@@ -1,6 +1,5 @@
-# Script to test the DAC chip that electronics has. Sends a cos wave to
-# channel 0 and a sin wave to channel 1. Should be pretty easily extensible
-# to test more channels and the second DAC. Wiring should be done in accorrdance
+# Script to test the DAC chips that electronics has. Sends a sine wave
+# to each active channel on each DAC. Wiring should be done in accordance
 # with our final plans for the Pi's pinout.
 
 import board
@@ -23,7 +22,7 @@ try:
     GPIO.output(GRN, GPIO.HIGH)
     while True:
         signal = math.floor(127 * (math.cos(i/100) + 1))
-        msg0 = [signal if i % 2 else reg0[i//2] for i in range(16)] #signal, reg0, signal, reg1, etc for DAC0
+        msg0 = [signal if i % 2 else reg0[i//2] for i in range(16)] #signal, channel0, signal, channel1, etc. for DAC0
         msg1 = [signal if i % 2 else reg1[i//2] for i in range(8)] #same for DAC1
         i2c.writeto(ad0, bytes(msg0))
         i2c.writeto(ad1, bytes(msg1))
@@ -32,7 +31,7 @@ try:
 except IOError as e:
     GPIO.output(RED, GPIO.HIGH)
     GPIO.output(GRN, GPIO.LOW)
-    time.sleep(1)
+    time.sleep(1) #maybe dumb; allows red LED to be on for 1 sec when an error is caught
     raise e
 finally:
     GPIO.output(RED, GPIO.LOW)
