9501bee3d9690afb3a3d327cdf8574693c755538
evanrittner <evanrittner@gmail.com>
2021-01-29 10:27:35 -0500

    More subprocess testing

--- Pi testing/subprocess_testing.py
+++ Pi testing/subprocess_testing.py
@@ -1,12 +1,30 @@
 import subprocess
+import time
 
 host = "FEMTAsimboxPi.local"
 user = "pi"
-#password = "FEMTAsoftwareFYEraspberryPI2020."
-command = "cd Project_Files; ls"
 
-args = f"ssh {user}@{host} '{command}'".split()
+def ssh_test(cmd):
+    arg = f"ssh {user}@{host} \"{cmd}\""
+    print(arg)
+    sp = subprocess.run(arg, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    print(f"command: {cmd} \n\nstdout: {sp.stdout.decode()} \n\nstderr: {sp.stderr.decode()}")
 
-sp = subprocess.run(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+#ssh_test("cd 'Project_Files; ls")
 
-print(f"stdout: {sp.stdout.decode()} \n\nstderr: {sp.stderr.decode()}")
+def comms_test():
+    arg1 = f"ssh {user}@{host} \"cd \\\"Project_Files/Pi testing\\\"; python ssh_leds.py\""
+    print(arg1)
+    sp1 = subprocess.Popen(arg1,  stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    time.sleep(3)
+    arg2 = f"ssh {user}@{host} \"cd \\\"Project_Files/Pi testing\\\"; echo \\\"1\\\" > state\""
+    print(arg2)
+    sp2 = subprocess.run(arg2, stdout=subprocess.PIPE, stderr=subprocess.PIPE, timeout=5)
+    sp1.wait(10)
+    print(f"stdout: {sp1.stdout.read().decode()} \nstderr: {sp1.stderr.read().decode()}")
+    print(f"stdout: {sp2.stdout.decode()} \nstderr: {sp2.stderr.decode()}")
+    assert sp1.poll() is None
+    arg3 = f"ssh {user}@{host} \"cd \\\"Project_Files/Pi testing\\\"; rm state; touch state"
+    sp3 = subprocess.run(arg3, stdout=subprocess.PIPE, stderr=subprocess.PIPE, timeout=5)
+
+comms_test()
