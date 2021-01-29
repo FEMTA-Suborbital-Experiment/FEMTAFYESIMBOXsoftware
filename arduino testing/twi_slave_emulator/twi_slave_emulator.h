a6b43a7a4ade57d1cac63a6486290084341962ae
Ethan Kessel <eqkessel@gmail.com>
2021-01-29 00:09:04 -0500

    Moved library files into sketch and modified to capture address
    Todo: cleanup

--- arduino testing/twi_slave_emulator/twi_slave_emulator.h
+++ arduino testing/twi_slave_emulator/twi_slave_emulator.h
@@ -53,7 +53,8 @@ SOFTWARE.
 #ifndef TWI_SLAVE_EMULATOR_H  //  #include protection
 #define TWI_SLAVE_EMULATOR_H
 
-#include <Wire.h> //  Arduino library for twi interface
+//#include <Wire.h> //  Arduino library for twi interface
+#include "src/Wire/Wire.h"
 
 ///////////////////////////////////////////////////////////////////////////////
 //  TYPE AND CLASS DECLARATIONS ///////////////////////////////////////////////
@@ -99,7 +100,7 @@ protected:  //  PROTECTED MEMBERS
 
   // Static functions passed to Wire.h
   static void requestHandler(void);  //  Internal functions to hook to TwoWire system
-  static void receiveHandler(int nBytes);
+  static void receiveHandler(int nBytes, uint8_t last_addr);
 
   // Member functions
   void setRegisters(void);
@@ -302,7 +303,7 @@ void TWI_SlaveEmulator<N>::requestHandler(void)
 //  Performs internal handling of a TWI bus recieve at an address we are listening to when called
 //  by Wire.h. See attachAddressReceive() for more information on how this is used.
 template <size_t N>
-void TWI_SlaveEmulator<N>::receiveHandler(int nBytes)
+void TWI_SlaveEmulator<N>::receiveHandler(int nBytes, uint8_t last_addr)
 {
   uint8_t data_register = TWDR; // Immediately capture address targeted in data register
   
@@ -314,7 +315,7 @@ void TWI_SlaveEmulator<N>::receiveHandler(int nBytes)
   {
     Serial.print(">>DEBUG>> TWDR is ");
     Serial.print(TWDR, BIN);
-    uint8_t last_addr = TWDR >> 1;  //  Get the address that was called (sitting in the data register)
+//    uint8_t last_addr = TWDR >> 1;  //  Get the address that was called (sitting in the data register)
     Serial.print(" last_addr is ");
     Serial.print(last_addr, BIN);
     Serial.print(" 0x");
@@ -322,15 +323,15 @@ void TWI_SlaveEmulator<N>::receiveHandler(int nBytes)
     Serial.print(" TWDR is ");
     Serial.print(TWDR, BIN);
 
-    last_addr = data_register >> 1; // Isolate 7-bit address from r/w bit
-    Serial.print(" -- last_addr is ");
-    Serial.print(last_addr, BIN);
-    Serial.print(" 0x");
-    Serial.print(last_addr, HEX);
-    Serial.print(" TWDR was ");
-    Serial.print(data_register, BIN);
-    Serial.print(" 0x");
-    Serial.print(data_register, HEX);
+//    last_addr = data_register >> 1; // Isolate 7-bit address from r/w bit
+//    Serial.print(" -- last_addr is ");
+//    Serial.print(last_addr, BIN);
+//    Serial.print(" 0x");
+//    Serial.print(last_addr, HEX);
+//    Serial.print(" TWDR was ");
+//    Serial.print(data_register, BIN);
+//    Serial.print(" 0x");
+//    Serial.print(data_register, HEX);
     uint8_t index = indexOfAddress(last_addr);
     if (index < _num_addresses)
     {
