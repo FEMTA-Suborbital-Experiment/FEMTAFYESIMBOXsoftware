7207bcc2047306d5eecd2e7383ec3dc777eec149
evanrittner <evanrittner@gmail.com>
2021-02-15 00:12:18 -0500

    Finished (?) config parser

--- Production/config_parser.py
+++ Production/config_parser.py
@@ -1,36 +1,39 @@
 # Parser for config.txt. This will return 'machine-readable' configurations
 # to the main script from a human-readable file.
 
-class ConfigurationError(SyntaxError):
+class ConfigurationError(Exception):
     """Errors raised when parsing config file"""
     def __init__(self, message):
         self.message = message
 
-with open("config.txt", "r") as c:
-    src = c.read().split("::")[1::2]
+try:
+    with open("Production/config.txt", "r") as cfg: #maybe need to modify path here for the pi?
+        src = cfg.read().split("::")[2::2]
+except FileNotFoundError:
+    raise ConfigurationError("The configuration file was not found")
 
 configs = dict() #To be filled and outputted
 
 
-#Parse simbox frequency
+# -=-=- Parse simbox frequency -=-=-
 try:
     configs["freq"] = int(src[0].strip().split()[0])
 except TypeError:
-    raise ConfigurationError("'Simbox Frequency' is missing or formatted incorectly")
+    raise ConfigurationError("'Simbox Frequency' is missing or formatted incorrectly")
 
 
-#Parse event timing
+# -=-=- Parse event timing -=-=-
 events = ["Launch", "Start of 0g", "Apogee", "End of 0g", "Landing"]
 lines = src[1].strip().split("\n")
 
 times = list()
 for i in range(5):
-    line = lines[i].split()
+    line = lines[i].split(";")
     if line[0] != events[i]:
-        raise ConfigurationError(f"Event name for flight event \#{i + 1} has been modified")
+        raise ConfigurationError(f"Event name for flight event #{i + 1} has been modified")
 
     try:
-        times.append(float(line[-1]))
+        times.append(float(line[-1].strip()))
     except TypeError:
         raise ConfigurationError(f"Non-numeric event time for event {line[0]}")
 
@@ -39,15 +42,16 @@ if times[0] != 0:
 elif len(times) != len(events):
     raise ConfigurationError("Flight event list has been modified")
 
+exp_duration = times[3] - times[1]
 configs["event_times"] = tuple(times)
 
 
-#Parse sensor failures
+# -=-=- Parse sensor failures -=-=-
 sensors = ["flow0", "flow1", "uv", "pres0", "pres1", "pres2", "pres3", "therm0",
 "therm1", "therm2", "therm3", "therm4", "mass0", "mass1", "ir0", "ir1"]
-states = ["normal", "dead", "min", "max"]
+states = ["normal", "min", "max", "dead"]
 
-#First, get a list of tuples of the form (name, state, time), where name and state are numeric
+# First, get a list of tuples of the form (name, state, time), where name and state are numeric
 failures = src[2].strip().split("\n")
 failures = [[i.strip() for i in line.split(";")][:3] for line in failures]
 
@@ -64,7 +68,7 @@ for i in range(len(failures)):
 
     if failures[i][1].lower() in states:
         try:
-            failures[i][1] = sensors.index(failures[i][1].lower())
+            failures[i][1] = states.index(failures[i][1].lower())
         except ValueError:
             raise ConfigurationError("Invalid sensor failure state")
     elif failures[i][1].isnumeric() and int(failures[i][1]) in range(4):
@@ -72,15 +76,84 @@ for i in range(len(failures)):
     else:
         raise ConfigurationError("Invalid sensor failure configuration")
 
-    if failures[i][1] == 1 and failures[i][0] > 2:
-        raise ConfigurationError("Only digital sensors can fail to state Dead")
+    if failures[i][1] == 3 and failures[i][0] > 2:
+        raise ConfigurationError(f"Only digital sensors can fail to state Dead (line {i + 1})")
 
     try:
         failures[i][2] = float(failures[i][2])
     except ValueError:
-        raise ConfigurationError(f"Invalid time for sensor failure \#{i + 1}")
-    if failures[i][2] < 0:
-        raise ConfigurationError(f"Invalid time for sensor failure \#{i + 1}")
+        raise ConfigurationError(f"Invalid time for sensor failure #{i + 1}")
+    
+    if 0 > failures[i][2]:
+        raise ConfigurationError(f"Invalid time for sensor failure #{i + 1} (before experiment start)")
+    if failures[i][2] > exp_duration:
+        raise ConfigurationError(f"Invalid time for sensor failure #{i + 1} (after experiment end)")
 
-#Next, convert digital sensors to error states
+failures.sort(key=lambda x: x[2])
 
+# Next, convert digital sensor 'dead' failures to error states
+dead_failures = [failure for failure in failures if (failure[0] < 3) and (failure[1] in (0, 3))]
+error_states = [[0, exp_duration, 0]] #-> start_t, end_t, event state
+for fail in dead_failures:
+    sensor = 2 ** fail[0]
+    state = bool(fail[1]) # i.e 1 if Dead, 0 if Normal
+    time = fail[2]
+
+    indx = -1
+    for i in range(len(error_states)):
+        if (error_states[i][0] < time) and (error_states[i][1] == exp_duration or error_states[i+1][0] > time):
+            # insert a new tuple starting at transition time and taking the previous tuple's end time
+            error_states.insert(i + 1, [time, error_states[i][1], error_states[i][2]])
+            # set previous tuple's end time to transition time
+            error_states[i][1] = time 
+            indx = i
+            break
+        elif error_states[i + 1][0] == time:
+            # no need for a new tuple
+            indx = i
+            break
+
+    # bitwise xor with new state for each tuple, starting from the new one,
+    # but only if there is a toggle to be done
+    if (error_states[indx][2] & sensor) != state:
+        for i in range(indx + 1, len(error_states)):
+            error_states[i][2] ^= sensor
+
+configs["dig_error_states"] = error_states
+
+# Next, parse normal/min/max for all sensors 
+# outputted: list of (start_t, end_t, <state>) like above but <state> is a 
+# 15-list with each spot corresponding to a sensor and equal to either 
+# 0, 1, or 2 (being Normal, Min, and Max)
+
+all_failures = [f for f in failures if f[1] != 3]
+error_states = [[0, exp_duration, [0]*15]] #-> start_t, end_t, state
+
+for fail in all_failures:
+    sensor = fail[0]
+    state = fail[1]
+    time = fail[2]
+
+    indx = -1
+    for i in range(len(error_states)):
+        if (error_states[i][0] < time) and (error_states[i][1] == exp_duration or error_states[i+1][0] > time):
+            # insert a new tuple starting at transition time and taking the previous tuple's end time
+            error_states.insert(i + 1, [time, error_states[i][1], error_states[i][2].copy()])
+            # set previous tuple's end time to transition time
+            error_states[i][1] = time 
+            indx = i
+            break
+        elif error_states[i + 1][0] == time:
+            # no need for a new tuple
+            indx = i
+            break
+
+    # update all following tuples with new state info
+    for i in range(indx + 1, len(error_states)):
+        error_states[i][2][sensor] = state
+
+configs["all_error_states"] = error_states
+
+
+if __name__ == "__main__":
+    print(configs)
