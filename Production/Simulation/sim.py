8f125d44323362d213b94762613ce3e7a2fdc759
evanrittner <evanrittner@gmail.com>
2021-02-20 17:00:44 -0500

    Barebones VE file

---
+++ Production/Simulation/sim.py
@@ -0,0 +1,17 @@
+# Main virtual environment simulation file.
+
+import multiprocessing.shared_memory as sm
+
+import numba
+
+from .constants import *
+from .helpers import nvcVP, nvcRho, waterVP, HerKnu, waterHV, mDotThruOrifice
+
+#define variables here
+altitude = 0
+
+def loop():
+    pass
+
+while altitude:
+    loop()
