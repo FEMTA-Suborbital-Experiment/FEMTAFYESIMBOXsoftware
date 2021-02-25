73f3cde80add8199bc000fed9035c32552f404b6
LavZed <jacob.a.valdez2002@gmail.com>
2021-02-24 22:45:13 -0500

    Labeled inner functions in main loop to prevent numba from running and generating on "much slower code."

--- Production/add_noise.py
+++ Production/add_noise.py
@@ -1,6 +1,9 @@
 # A nice little function to add random noise to the sensor data.
 
 from random import gauss
+from numba import jit, float32
 
+
+@jit(float32)
 def fuzz(value, factor=0.01):
     return gauss(value, factor * value)
