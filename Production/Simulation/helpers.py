b9a22ae48f2150217bb7db70b0f5746cd499aa0d
LavZed <jacob.a.valdez2002@gmail.com>
2021-03-01 18:03:20 -0500

    Converted numba function signatures for helper and add_noise functions to double-precision

--- Production/Simulation/helpers.py
+++ Production/Simulation/helpers.py
@@ -1,21 +1,22 @@
 # Set of functions called from sim.py
 
 import numpy as np
-from numba import jit, float32
+from numba import njit, float64
 from constants import *
 
 
 # fastmath allows floating-point operations to be performed in an unsafe matter, sacrificing accuracy for speed
 toggle_fastmath = True
 
+
 # Vapor Pressure of NV 7100 (T in K, vp in Pa)
-@jit(float32(float32), fastmath=toggle_fastmath, nopython=True)
+@njit(float64(float64), fastmath=toggle_fastmath)
 def nvcVP(T):
     return np.exp(22.415 - 3641.9 * (1 / T))
 
 
 # Density of NV 7100 Liquid (T in K)
-@jit(float32(float32), fastmath=toggle_fastmath, nopython=True)
+@njit(float64(float64), fastmath=toggle_fastmath)
 def nvcRho(T):
     dD = 1.5383 - 0.002269 * (T - 273.15)
     dD /= (1000 * 0.000001) #kg/m^3
@@ -23,7 +24,7 @@ def nvcRho(T):
 
 
 # Vapor Pressure of Water (T in K, vp in Pa)
-@jit(float32(float32), fastmath=toggle_fastmath, nopython=True)
+@njit(float64(float64), fastmath=toggle_fastmath)
 def waterVP(T):
     vp = 10 ** (8.07131 - (1730.63 / (233.426 + (T - 273)))) #pressure in mmHg
     vp *= 133 #conversion to Pa from mmHg
@@ -33,14 +34,14 @@ def waterVP(T):
 # Mass transfer from gas to liquid [kg/s] (HERTZ-KNUDSEN EQUATION)
 # Negative denotes vapor to liquid (condensation), Positive denotes liquid to vapor
 # (evaporation)
-@jit(float32(float32, float32, float32, float32, float32, float32, float32, float32, float32), fastmath=toggle_fastmath, nopython=True)
+@njit(float64(float64, float64, float64, float64, float64, float64, float64, float64, float64), fastmath=toggle_fastmath)
 def HerKnu(Ps, T_liquid, T_vapor, Pg, m, A_evap, A_cond, C_evap, C_cond):
     m_transfer = np.sqrt(m / (2 * pi * kB)) * ((A_evap * C_evap * (Ps / np.sqrt(T_liquid))) - (A_cond * C_cond * (Pg / np.sqrt(T_vapor))))
     return m_transfer
 
 
 # Heat of Vaporization of Water [J/kg]
-@jit(float32(float32), fastmath=toggle_fastmath, nopython=True)
+@njit(float64(float64), fastmath=toggle_fastmath)
 def waterHV(T):
     Hvs = np.array([2500.9, 2496.2, 2491.4, 2477.2, 2467.7, 2458.3, 2453.5, 2441.7, 2429.8, 2420.3, 2406, 2396.4, 2381.9, 2372.3, 2357.7, 2333, 2308, 2282.5, 2266.9, 2256.4, 2229.6, 2202.1, 2144.3, 2082, 2014.2, 1939.7, 1857.4, 1765.4, 1661.6, 1543, 1404.6, 1238.4, 1027.3, 719.8])
     Ts = 273 + np.array([0.00, 2, 4, 10, 14, 18, 20, 25, 30, 34, 40, 44, 50, 54, 60, 70, 80, 90, 96, 100, 110, 120, 140, 160, 180, 200, 220, 240, 260, 280, 300, 320, 340, 360])
@@ -49,7 +50,7 @@ def waterHV(T):
 
 # mDotThruOrifice calculates mass flow (mDot) in kg/s through an orifice
 # given pressures and working fluid properties
-@jit(float32(float32, float32, float32, float32, float32, float32), fastmath=toggle_fastmath, nopython=True)
+@njit(float64(float64, float64, float64, float64, float64, float64), fastmath=toggle_fastmath)
 def mDotThruOrifice(in1, in2, rho, gamma, outletCD, outletDia):
 #   Variable description:
 #       in1: pressure upstream (Pa)
@@ -87,7 +88,8 @@ def mDotThruOrifice(in1, in2, rho, gamma, outletCD, outletDia):
     mDot = outletCD * outletArea * np.sqrt(upP * rho * (2 * gamma / (gamma-1)) * np.pow(r, (2 / gamma)) * (1 - np.pow(r, ((gamma - 1) / gamma)))) #kg/s
     return mDot * direction #Corrects sign on mDot to follow stated convention above
 
-@jit(float32(float32), fastmath=toggle_fastmath, nopython=True)
+
+@njit(float64(float64), fastmath=toggle_fastmath)
 def StandardAtm(h):
     rho_sea = 1.2250     # density at sea level, kg/ m^3
     p_sea   = 1.01325e5  # pressure at sea level, N/m^2
--- Production/add_noise.py
+++ Production/add_noise.py
@@ -1,9 +1,9 @@
 # A nice little function to add random noise to the sensor data.
 
 from random import gauss
-from numba import jit, float32
+from numba import jit, float64
 
 
-@jit(float32)
+@jit(float64)
 def fuzz(value, factor=0.01):
     return gauss(value, factor * value)
