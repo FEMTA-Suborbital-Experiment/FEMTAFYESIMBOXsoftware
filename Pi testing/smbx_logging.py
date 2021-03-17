a30fedf680453b24ba56057842588efe8288cc44
evanrittner <evanrittner@gmail.com>
2021-03-17 11:47:07 -0400

    Reworking and cleaning up logger

--- Pi testing/smbx_logging.py
+++ Pi testing/smbx_logging.py
@@ -6,42 +6,38 @@ class Logger:
     instances = 0
     file_objects = dict()
 
-    # Generator for unique instance IDs
-    @staticmethod
-    def __id_gen():
-        next_id = 0
-        while True:
-            yield next_id
-            next_id += 1
-    __id = __id_gen.__func__()  # Build generator object from function
-
-    def __init__(self):
-        print("in __init__")
+    def __init__(self, name):
+        self.name = name
 
     def __enter__(self):
-        print("in __enter__")
-        # Members of self.__class__ provides implementation of "static" members--shared between all instances of this class
-        self.id = next(self.__class__.__id)
-        self.__class__.instances += 1
+        Logger.instances += 1
         return self
     
     def __exit__(self, exc_type, exc_val, exc_tb):
-        self.__class__.instances -= 1
-        if not self.__class__.instances:    # No more active instances remaining, i.e. we're last to be shut down
-            for fo in self.__class__.file_objects.values():
+        Logger.instances -= 1
+        if not Logger.instances:    # No more active instances remaining, i.e. we're last to be shut down
+            for fo in Logger.file_objects.values():
                 fo.close()
         return None
 
-    def write(self, text, filename, _print=False) -> None:
-        if filename not in self.__class__.file_objects.keys():
-            self.__class__.file_objects[filename] = open(filename, "a")
+    def write(self, text, filename, _print=False):
+        if filename not in Logger.file_objects.keys():
+            # open with mode x because we neither want to overwrite a previous log nor combine two logs together
+            try:
+                Logger.file_objects[filename] = open(filename, "x", encoding="ascii")
+            except FileExistsError as e:
+                raise Exception(f"log file \"{filename}\" already exists") from e 
         
-        self.__class__.file_objects[filename].write(text)
+        Logger.file_objects[filename].write(self.name + ": " + text + "\n")
 
         if _print:
+            # Print logged message to terminal. These should be infrequent and
+            # straightforward, so no need to incluse the logger name
             print(text)
 
 
 if __name__ == '__main__':
-    with Logger() as log1, Logger() as log2:
-        print(log1.id, log2.id)
+    with Logger("main") as log1, Logger("arduino") as log2:
+        print(log1.name, log2.name)
+        log1.write("Hello World!", "test.txt")
+        log2.write("Hello again", "test.txt", _print=True)
