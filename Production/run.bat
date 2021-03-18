c5c0968ccb9f7015e4f7194503600c8e71932251
evanrittner <evanrittner@gmail.com>
2021-03-17 23:15:50 -0400

    Updated run.bat given new pi Python setup

--- Production/run.bat
+++ Production/run.bat
@@ -1,3 +1,3 @@
 @ECHO OFF
 scp config.txt pi@FEMTAsimboxPi.local:Project_Files/Production
-ssh pi@FEMTAsimboxPi.local "cd Project_Files/Production; python main.py"
+ssh pi@FEMTAsimboxPi.local "activate; cd Project_Files/Production; python3.8 main.py"
