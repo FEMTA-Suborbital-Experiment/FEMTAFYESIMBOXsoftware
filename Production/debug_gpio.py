0a096317b8944a5daba9b7d02d85862235df7459
evanrittner <evanrittner@gmail.com>
2021-03-21 20:52:15 -0400

    Made input method of debug GPIO function correctly

--- Production/debug_gpio.py
+++ Production/debug_gpio.py
@@ -1,5 +1,6 @@
 # Stand-in GPIO class for debug mode testing
 
+import time
 from smbx_logging import Logger
 
 class GPIO:
@@ -8,15 +9,28 @@ class GPIO:
     HIGH = True
     LOW = False
     _pins = dict()
+    start_t = time.time() #not exactly correct, but good enough
 
     @classmethod
     def setup(cls, pin_num, mode):
         cls._pins[pin_num] = mode
 
     @classmethod
+    def _get_new_state(cls, pin, t): #conditions taken from matlab source
+        time_diff = time.time() - t
+        if pin == 4: #first pin, we're calling it the first flow solenoid
+            return cls.LOW if time_diff < 168.385 else cls.HIGH
+        elif pin == 14: #second flow sol
+            return cls.HIGH if 168.385 < time_diff < 200 else cls.LOW
+        elif pin == 15: #vent sol
+            return cls.HIGH
+        else:
+            return cls.LOW
+
+    @classmethod
     def input(cls, pin):
         if pin in cls._pins.keys() and cls._pins[pin] == cls.IN:
-            new_state = cls.HIGH #TODO: how do we chose GPIO input?
+            new_state = cls._get_new_state(pin, time.time())
             with Logger("Debug") as log:
                 log.write(f"GPIO {pin} input {'high' if new_state else 'low'}", "low_freq.txt")
             return new_state
