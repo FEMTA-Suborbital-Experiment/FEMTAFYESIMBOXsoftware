210f459c4bd28d84cd57776d74049cee4d1dc8e6
evanrittner <evanrittner@gmail.com>
2021-03-22 22:58:59 -0400

    Little change to syntax for readability

--- Production/smbx_logging.py
+++ Production/smbx_logging.py
@@ -50,7 +50,7 @@ class Logger:
             for fo in Logger.file_objects.values():
                 fo.close()
 
-    def write(self, text, filename, _print=False, **print_kwargs):
+    def write(self, text, filename, print_=False, **print_kwargs):
         assert self.start_t is not None, f"Logger \"{self.name}\" has not had \"start()\" called"
 
         if filename not in Logger.file_objects.keys():
@@ -63,7 +63,7 @@ class Logger:
         
         Logger.file_objects[filename].write(f"{self.name} [{time.perf_counter() - self.start_t}]: {text!a}\n")
 
-        if _print:
+        if print_:
             # Print logged message to terminal. These should be infrequent and
             # straightforward, so no need to include the logger name and time
             print(text, **print_kwargs)
@@ -73,4 +73,5 @@ if __name__ == '__main__':
     with Logger("main") as log1, Logger("arduino") as log2:
         print(log1.name, log2.name)
         log1.write("Hello World!", "test.txt")
-        log2.write("Hello again", "test.txt", _print=True)
+        log2.write("Hello again", "test.txt", True)
+        log1.write("Print kwargs:", "test.txt", True, end="<end str>\n")
