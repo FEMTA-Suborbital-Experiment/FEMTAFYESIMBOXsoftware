ab5a8043f563ad786696f5fbea54d460808efbba
evanrittner <evanrittner@gmail.com>
2021-04-12 14:08:08 -0400

    Few more file transfers

--- Pi testing/serial_float_test.py
+++ Pi testing/serial_float_test.py
@@ -11,6 +11,10 @@
 import serial
 import time
 
+"""NOTE: Python uses doubles for all floats (that we'll be using?)
+   Apparently the best way to pass floats/doubles over serial is to
+   convert them to strings. (Though that feels gross.)"""
+
 if __name__ == '__main__':
     s = serial.Serial('/dev/ttyACM0', baudrate=115200, timeout=1)
     data = [0.00, 1.11, 2.22, 3.33, 4.44, 5.55, 6.66, 7.77, 8.88, 9.99]
