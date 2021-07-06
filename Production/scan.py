ef0dd9ae48ab7840c9b55176479fafdc9d972092
evanrittner <evanrittner@gmail.com>
2021-07-06 15:30:31 -0500

    Fixed issue with too many processes launched

--- Production/scan.py
+++ Production/scan.py
@@ -2,6 +2,7 @@
 # i.e. using the arg "192.168.1.0/24" will ping addresses from 192.168.1.0 to 192.168.1.255
 
 import sys
+import time
 import subprocess
 
 
@@ -12,6 +13,9 @@ address, mask_bits = sys.argv[1].split("/")
 address = int("".join([f"{int(byte):02x}" for byte in address.split(".")]), 16)
 mask_bits = int(mask_bits)
 
+if not (0 <= mask_bits <= 32):
+    raise RuntimeError("invalid mask")
+
 
 def num_to_ip(num):
     num = hex(num)[2:]
@@ -19,6 +23,9 @@ def num_to_ip(num):
 
 
 for incr in range(2 ** (32 - mask_bits)):
+    if 32 - mask_bits > 4 and incr % 8 == 0:
+        time.sleep(3) # Kind of janky protection against too many processes
+        
     command = ["ping", "-n", "1", num_to_ip(address + incr)]
     subprocess.Popen(command, stdout=subprocess.DEVNULL)
     print(" ".join(command))
